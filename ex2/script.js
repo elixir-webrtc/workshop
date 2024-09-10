11fe5ce87da3e38078a91bb00c83fe07d4a8b6ca
Jakub Pisarek <99591440+sgfn@users.noreply.github.com>
2024-09-10 15:22:46 +0200

    Presenter branch

--- ex2/script.js
+++ ex2/script.js
@@ -2,26 +2,32 @@ const localPlayer = document.getElementById('localPlayer');
 const remotePlayer = document.getElementById('remotePlayer');
 (async function () {
   // obtain access to the user's audio and video
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+  });
 
   // pin the stream to the localPlayer
+  localPlayer.srcObject = stream;
 
   // create two peer connections
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/RTCPeerConnection
+  const pc1 = new RTCPeerConnection();
+  const pc2 = new RTCPeerConnection();
 
   // add tracks to pc1
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/addTrack
+  stream.getTracks().forEach(track => pc1.addTrack(track, stream));
 
   // create and set offer
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createOffer
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/setLocalDescription
+  const offer = await pc1.createOffer();
+  await pc1.setLocalDescription(offer);
 
   // set offer on the other side
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/setRemoteDescription
+  await pc2.setRemoteDescription(offer);
 
   // create and set answer
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createAnswer
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/setLocalDescription
+  const answer = await pc2.createAnswer();
+  await pc2.setLocalDescription(answer);
 
   // set answer on the other side
-  // use https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/setRemoteDescription
+  await pc1.setRemoteDescription(answer);
 })();
